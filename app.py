057dfec4743d60fe38146a842a1f52cbe1cf1bda
Alexander <sasha.izmyloff@mail.ru>
2024-12-07 09:53:39 +0300

    Stream Page

--- app.py
+++ app.py
@@ -20,6 +20,19 @@ def index():
             return render_template("index.html", paths=res, live_path=app.live_host)
     return {"error": f"API {app.api_host} not available"}
 
+@app.get("/stream/watch")
+def stream_watch():
+    path = request.args.get('path')
+    if path:
+        return render_template("stream.html", path=path, live_path=app.live_host)
+    return {"error": "Stream not found!"}
+
+@app.get("/msg/list")
+def msg_list():
+    pass
+@app.post("/msg/send")
+def msg_send():
+    pass
 
 if __name__ == '__main__':
     app.run()
