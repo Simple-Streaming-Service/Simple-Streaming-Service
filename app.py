a93c37cb099d1a319a1c2393a47ba8d2eb528183
ReLLoMine <slaval.125.sl@gmail.com>
2024-12-08 23:59:47 +0300

    added env vars fixed README.md

--- app.py
+++ app.py
@@ -5,20 +5,39 @@ from flask import request, jsonify, redirect
 import requests
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
-app.secret_key = "super secret key"
-app.api_host = os.getenv("API_HOST", "http://localhost:9997")
-app.live_host = os.getenv("LIVE_HOST", "http://localhost:8888")
+app.secret_key = os.getenv("SECRET_KEY", "secret")
+
+# MediaMTX
+app.mtx_api_host = os.getenv("MTX_API_HOST", "localhost")
+app.mtx_api_host_port = os.getenv("MTX_API_HOST_PORT", "9997")
+app.mtx_api_uri = app.mtx_api_host + ":" + app.mtx_api_host_port
+
+# MediaMTX HLS
+app.mtx_hls_host = os.getenv("MTX_LIVE_HOST", "localhost")
+app.mtx_hls_host_port = os.getenv("MTX_LIVE_HOST_PORT", "8888")
+app.mtx_hls_uri = app.mtx_hls_host + ":" + app.mtx_hls_host_port
+
+
+# MongoDB
+app.mongo_host = os.getenv("MONGO_HOST", "localhost")
+app.mongo_port = os.getenv("MONGO_PORT", "27017")
+app.mongo_user = os.getenv("MONGO_USER", "root")
+app.mongo_password = os.getenv("MONGO_PASSWORD", "root")
+
+
+def get_mongo_uri():
+    return f"mongodb://{app.mongo_user}:{app.mongo_password}@{app.mongo_host}:{app.mongo_port}"
 
 
 @app.get("/")
 def index():
-    r = requests.get(app.api_host + "/v3/paths/list")
+    r = requests.get(app.mtx_api_uri + "/v3/paths/list")
     if r.status_code == 200:
         data = r.json()
         if "items" in data:
             res = [x["name"] for x in data["items"]]
-            return render_template("index.html", paths=res, live_path=app.live_host)
-    return {"error": f"API {app.api_host} not available"}
+            return render_template("index.html", paths=res, live_path=app.mtx_hls_uri)
+    return {"error": f"MediaMTX API on {app.mtx_api_uri} not available"}
 
 
 if __name__ == '__main__':
